1405df1c07b3b0c6e796e461f5ecd123b2b9d256
Lenslow <134888265+iLenslow@users.noreply.github.com>
2025-05-03 09:57:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ document.getElementById("entryForm").addEventListener("submit", function(event) 
         params.push(`${encodeURIComponent(paramName)}=${encodeURIComponent(paramValue)}`);
     });
 
-    const baseUrl = "http://192.168.101.110/dw/trigger";
+    const baseUrl = "http://Placeholder.lenslow/dw/trigger";
     const finalUrl = `${baseUrl}/${selection}?${params.join("&")}`;
 
     console.log("🌐 Generierte URL:", finalUrl);
